2c210d480f7db578dbe2056f02e5d4cb7ee3bd7b
zhangsen <zhangsen@3dnest.cn>
2023-05-18 16:27:54 +0800

    workspace: [FIXED] change geometry folder location

--- packages/wall-material-lyj/src/utils.js
+++ packages/wall-material-lyj/src/utils.js
@@ -1,5 +1,5 @@
 import { COLOR } from './constant.js';
-import { Vector, Tuple, Point, Line } from '../../../script/geometry/geometry.min.js';
+import { Vector, Tuple, Point, Line } from './geometry/geometry.min.js';
 
 export const getColors = (context) => {
 	const { hover, focus, select, virtual, /* hold */ } = context;
