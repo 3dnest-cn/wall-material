7b05c5fdcfbabec834cd5c05d51a8062526fc097
zhangsen <zhangsen@3dnest.cn>
2023-07-03 14:47:31 +0800

    @3dnest/wall-material-ajk: [FIXED] Bealock

--- packages/wall-material-ajk/src/Door/Bealock.js
+++ packages/wall-material-ajk/src/Door/Bealock.js
@@ -1,13 +1,20 @@
+import { LINE_WIDTH } from '../constant.js';
 import { getColors } from '../utils.js';
 
 export const Bealock = (graphics, context) => {
-	const { width, height} = context;
+	const { width, height, scale } = context;
 	const color = getColors(context);
+	const lineWidth = LINE_WIDTH / scale;
 
 	graphics
 		.beginFill(color.fill, .01)
 		.drawRect(0, 0, width, height)
-		.endFill();
+		.endFill()
+		.lineStyle(lineWidth, color.arcLine)
+		.moveTo(0, 0)
+		.lineTo(0, height)
+		.moveTo(width, 0)
+		.lineTo(width, height);
 
 	graphics.pivot.x = width / 2;
 	graphics.pivot.y = height / 2;
