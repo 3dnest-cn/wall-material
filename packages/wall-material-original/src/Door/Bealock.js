c038738abb41e57ba24bab909bf6c2b58a979a3d
zhangsen <zhangsen@3dnest.cn>
2023-08-16 20:58:52 +0800

    @3dnesst/wal-material-original: [FIXED] bealock width

--- packages/wall-material-original/src/Door/Bealock.js
+++ packages/wall-material-original/src/Door/Bealock.js
@@ -5,7 +5,7 @@ export const Bealock = (graphics, context) => {
 	const { width, height, scale} = context;
 	const { emptyAlpha } = STATE;
 
-	const rectWidth = 100 / scale;
+	const rectWidth = 40 / scale;
 	const color = getColors(context);
 
 	graphics
@@ -15,11 +15,11 @@ export const Bealock = (graphics, context) => {
 		.beginFill(color.line)
 		.drawRect(0, 0, rectWidth, height)
 		.beginHole()
-		.drawRect(rectWidth / 4, height / 6, rectWidth / 2, height * 2 / 3)
+		.drawRect(rectWidth / 4, rectWidth / 2, rectWidth / 2, height - rectWidth)
 		.endHole()
 		.drawRect(width - rectWidth, 0, rectWidth, height)
 		.beginHole()
-		.drawRect(width - rectWidth * 3 / 4, height / 6, rectWidth / 2, height * 2 / 3)
+		.drawRect(width - rectWidth * 3 / 4, rectWidth / 2, rectWidth / 2, height - rectWidth)
 		.endHole()
 		.endFill();
 
